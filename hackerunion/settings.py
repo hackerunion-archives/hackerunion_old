30bb992639c14802de2763cd97b889fd78efa9f1
Michael Dippery <mpd@nyhacker.org>
2011-11-06 14:17:47 -0500

    Install Django South app
    South is used for managing database migrations in Django.

--- hackerunion/settings.py
+++ hackerunion/settings.py
@@ -119,9 +119,14 @@ INSTALLED_APPS = [
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    
+    # Core apps
     'chapters',
     'people',
     'projects',
+    
+    # Third-party apps
+    'south',
 ]
 
 # A sample logging configuration. The only tangible logging
