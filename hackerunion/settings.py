8fa1ad71d79116752522b1a4d7dd9fbad44a901c
Michael Dippery <mpd@nyhacker.org>
2011-11-06 14:31:28 -0500

    Configure the development database
    For development, I'm using a SQLite database, which is created in the
    repo's "db" directory. Other users may want to use a different DB
    system, and once we deploy we'll have to have server-specific settings,
    so eventually this will likely be moved out of settings.py, but for
    now this will help jumpstart development.

--- hackerunion/settings.py
+++ hackerunion/settings.py
@@ -13,12 +13,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(PROJECT_DIR, os.path.pardir, 'db', 'hackerunion.db'),
     }
 }
 
