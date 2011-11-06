83a34a376a09dc65e8ccc26b2e67c82fe7bc01d7
Michael Dippery <mpd@nyhacker.org>
2011-11-06 14:34:55 -0500

    Hook up root URLs
    Obviously these are just placeholders for now.

---
+++ hackerunion/chapters/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls.defaults import patterns, url
+from django.views.generic.simple import direct_to_template
+
+urlpatterns = patterns('',
+    url(r'^$', direct_to_template, {'template': 'chapters/base.html'}, name='chapters'),
+)
--- hackerunion/urls.py
+++ hackerunion/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    url('^$', direct_to_template, {'template': 'base.html'}, name='homepage'),
+    
     (r'^chapters/', include('chapters.urls')),
     (r'^people/', include('people.urls')),
     (r'^projects/', include('projects.urls')),
