3a604c8cbd4bd6a69e1235b7e6f61843fdd6ce4d
Michael Dippery <mpd@nyhacker.org>
2011-11-06 20:01:42 -0500

    Create some basic unit tests
    "Basic" being the keyword here.

--- hackerunion/chapters/tests.py
+++ hackerunion/chapters/tests.py
@@ -1,16 +1,7 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class URLTestCase(TestCase):
+    def test_index(self):
+        resp = self.client.get('/chapters/')
+        self.assertEqual(resp.status_code, 200)
--- hackerunion/people/tests.py
+++ hackerunion/people/tests.py
@@ -1,16 +1,15 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class URLTestCase(TestCase):
+    fixtures = ['people.yaml']
+    
+    def test_index(self):
+        resp = self.client.get('/people/')
+        self.assertEqual(resp.status_code, 200)
+    
+    def test_profile(self):
+        resp = self.client.get('/people/jdraper/')
+        self.assertEqual(resp.status_code, 200)
+        resp = self.client.get('/people/doesnotexist/')
+        self.assertEqual(resp.status_code, 404)
