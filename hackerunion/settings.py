fe80e7affacbec37341ba3f09c0388d915ae33c7
Michael Dippery <mpd@nyhacker.org>
2011-11-11 12:09:22 -0500

    Configure defaults for media and static dirs

--- hackerunion/settings.py
+++ hackerunion/settings.py
@@ -51,7 +51,7 @@ USE_L10N = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_DIR, os.path.pardir, 'www', 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -62,7 +62,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_DIR, os.path.pardir, 'www', 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
