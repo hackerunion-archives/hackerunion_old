f0d68df71f78d2ba7ed52ef859ea0ead5bf75678
Michael Dippery <mpd@nyhacker.org>
2011-11-14 17:13:15 -0500

    Create user profile object when new users are created

--- hackerunion/people/models.py
+++ hackerunion/people/models.py
@@ -1,9 +1,19 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models import signals
 
 
 class HackerProfile(models.Model):
     user = models.OneToOneField(User)
+    
+    def __unicode__(self):
+        return u"Profile: %s" % self.user.username
 
     class Meta:
         verbose_name = 'profile'
+
+
+def create_profile(sender, *args, **kwargs):
+    user = kwargs['instance']
+    profile, created = HackerProfile.objects.get_or_create(user=user)
+signals.post_save.connect(create_profile, sender=User)
