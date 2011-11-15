36ee284524d25fa22b8dc138fa6304afb3dac405
Michael Dippery <mpd@nyhacker.org>
2011-11-14 22:37:41 -0500

    Operate on user profile when grabbing Gravatar image
    Everything else in the template uses the user profile, so this is
    more consistent.

--- hackerunion/people/templatetags/people.py
+++ hackerunion/people/templatetags/people.py
@@ -5,7 +5,7 @@ register = template.Library()
 
 
 @register.filter
-def gravatar_url(user):
-    cleaned_email = user.get_profile().contact_email.strip().lower()
+def gravatar_url(user_profile):
+    cleaned_email = user_profile.contact_email.strip().lower()
     email_hash = hashlib.md5(cleaned_email).hexdigest()
     return "http://www.gravatar.com/avatar/%s?d=mm" % email_hash
