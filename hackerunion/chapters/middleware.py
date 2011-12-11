e6ba133fc5fdc6dd6a8b02fcf78e3e35a6ae104f
Michael Dippery <mpd@nyhacker.org>
2011-12-11 16:26:03 -0500

    Allow chapter to be set manually in DEBUG mode
    You can set your chapter by appending "?c=<chapter>" in URLs. Debug
    chapters are set in a cookie, so you can drop the query string option
    from subsequent requests.
    
    This eases testing of chapter subdomains, since subdomains can't
    really be set in local development.

--- hackerunion/chapters/middleware.py
+++ hackerunion/chapters/middleware.py
@@ -1,16 +1,46 @@
+import datetime
+import logging
+from django.conf import settings
 from django.contrib.sites.models import Site
 from chapters.models import Chapter
+
+logger = logging.getLogger(__name__)
 
 
 class ChapterRoutingMiddleware(object):
     def process_request(self, request):
+        if settings.DEBUG:
+            return self.DEBUG_process_request(request)
         domain_name = request.META.get('SERVER_NAME')
         if not domain_name:
             domain_name = Site.objects.get_current().domain
         parts = domain_name.split('.')
         subdomain = parts[0]
+        chapter = None
         try:
             chapter = Chapter.objects.get(subdomain=subdomain)
         except Chapter.DoesNotExist:
-            chapter = None
+            pass
         request.chapter = chapter
+    
+    def DEBUG_process_request(self, request):
+        chapter = None
+        if request.COOKIES.get('use_domain'):
+            subdomain = request.COOKIES['use_domain']
+            try:
+                chapter = Chapter.objects.get(subdomain=subdomain)
+            except Chapter.DoesNotExist:
+                pass
+        request.chapter = chapter
+
+    def process_response(self, request, response):
+        if settings.DEBUG and request.GET.get('c'):
+            subdomain = request.GET['c']
+            if Chapter.objects.filter(subdomain=subdomain).exists():
+                age = datetime.timedelta(days=356)
+                response.set_cookie('use_domain', subdomain,
+                                    max_age=age.total_seconds())
+                logger.debug('Set chapter: %s', subdomain)
+            else:
+                logger.debug('Chapter %s does not exist', subdomain)
+        return response
