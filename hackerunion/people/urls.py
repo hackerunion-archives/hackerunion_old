06d2af3a59145ff5dbf93f9eb096897143c40f2c
Michael Dippery <mpd@nyhacker.org>
2011-12-12 13:40:17 -0500

    Remove unused URL declaration

--- hackerunion/people/urls.py
+++ hackerunion/people/urls.py
@@ -5,7 +5,6 @@ from people.views import *
 urlpatterns = patterns('',
     url(r'^$', people_list, name='people_index'),
     url(r'^signup/$', signup),
-    url(r'^signup_confirmation/$', direct_to_template, {'template': 'people/signup_confirmation.html'}),
     url(r'^(?P<userid>[0-9]+)/$', userid, name='people_userid'),
     url(r'^(?P<username>[A-Za-z0-9]+)/$', profile, name='people_profile'),
 )
